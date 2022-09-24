a6e4d35563a3739f9c884da9e571c3ea6d550fa7
alyfa <alyfadel67@gmail.com>
2022-09-24 17:16:28 +0200

    navigator from splash screen to onBoarding screen

--- lib/presenation/resources/assets_manager.dart
+++ lib/presenation/resources/assets_manager.dart
@@ -1,6 +1,10 @@
 
-const String IMAGE_PATH ='assets/images';
+const String imagePATH ='assets/images';
 class ImageAssets
 {
-  static const String splashScreen = '$IMAGE_PATH/splash_logo.png';
+  static const String splashScreen = '$imagePATH/splash_logo.png';
+  static const String onBoardingLogo1 = '$imagePATH/onboarding_logo1.svg';
+  static const String onBoardingLogo2 = '$imagePATH/onboarding_logo2.svg';
+  static const String onBoardingLogo3 = '$imagePATH/onboarding_logo3.svg';
+  static const String onBoardingLogo4 = '$imagePATH/onboarding_logo4.svg';
 }
